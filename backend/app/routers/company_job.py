583b758de2c65a1ad24ad924790323146db4998f
Amin Mahjoub <aminmahjoub@Amins-Laptop.local>
2025-03-01 16:45:06 -0800

    clean up code

--- backend/app/routers/company_job.py
+++ backend/app/routers/company_job.py
@@ -87,6 +87,16 @@ async def get_company_job_info(company: str, job_position: str):
                 "status": "<Validated|Needs Work|Incorrect>",
                 "content": "<Provide details on the salary range>",
                 "source": ["<url1>", "<url2>", ...]
+            }},
+            "leetcode": {{ #provide common leetcode questions for the position
+                "status": "<Validated|Needs Work|Incorrect>",
+                "content": "<Provide details on the salary range>",
+                "source": ["<url1>", "<url2>", ...]
+            }},
+            "behavioral": {{ #provide common behavioral questions for the position
+                "status": "<Validated|Needs Work|Incorrect>",
+                "content": "<Provide details on the salary range>",
+                "source": ["<url1>", "<url2>", ...]
             }}
         }}
         }}
@@ -112,7 +122,9 @@ async def get_company_job_info(company: str, job_position: str):
 		    "type": "json_schema",
         "json_schema": {"schema": JobInformation.model_json_schema()}
     }
-    prelixty_client = PerplexityClient(api_key=PERPLEXITY_API_KEY, api_url="https://api.perplexity.ai/chat/completions", model="sonar")
+    prelixty_client = PerplexityClient(api_key=PERPLEXITY_API_KEY, 
+                                       api_url="https://api.perplexity.ai/chat/completions", 
+                                       model="sonar")
     response = await prelixty_client.get_response(messages=messages, response_format=response_format)
     print(type(response))
     print(("Raw response from API: %r", response))
@@ -122,14 +134,12 @@ async def get_company_job_info(company: str, job_position: str):
         response = json.loads(cleaned)
     except Exception as e:
         print("ERROR RESPONSE", e)
-        # You can choose to raise an HTTPException or return an error response
         raise HTTPException(status_code=500, detail="Response validation failed")
 
     try:
         response_format = JobInformation(**response)
     except Exception as e:
         print("ERROR RESPONSE", e)
-        # You can choose to raise an HTTPException or return an error response
         raise HTTPException(status_code=500, detail="Response validation failed")
 
     return {"data": response_format}
