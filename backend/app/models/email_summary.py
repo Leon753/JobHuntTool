17740ac08034cd2a8fdb2bb1a4e3e3ccbe86af9c
Amin Mahjoub <aminmahjoub@Amins-Laptop.local>
2025-03-06 20:43:58 -0800

    updated reponse to get email summary

--- backend/app/models/email_summary.py
+++ backend/app/models/email_summary.py
@@ -1,4 +1,12 @@
 from pydantic import BaseModel
+from enum import Enum
+
+class Status(Enum):
+    IN_REVIEW = "InReview"
+    INTERVIEWING = "Interviewing"
+    OFFER = "Offer"
+    REJECTED = "Rejected"
+
 
 class Email_Summary_Request(BaseModel):
     content: str
@@ -7,3 +15,4 @@ class GPT_Email_Summary_Response(BaseModel):
     summary: str
     company: str
     job_position: str
+    status: Status
